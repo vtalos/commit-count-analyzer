fafc85ee8ea19b1c17620ee55b470d41aca6ab80
vtalos <t8210146@aueb.gr>
2023-05-12 16:46:42 +0300

    Add csv contents in lists

--- commit_analyzer.py
+++ commit_analyzer.py
@@ -1,1 +1,27 @@
-print("Hello World!")
+import csv
+
+# create empty lists for each time period
+period1 = []
+period2 = []
+period3 = []
+period4 = []
+period5 = []
+
+# open the csv file and read its contents into the lists
+with open("percentages.csv") as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # skip header row
+    for row in reader:
+        period1.append(float(row[0]))
+        period2.append(float(row[1]))
+        period3.append(float(row[2]))
+        period4.append(float(row[3]))
+        period5.append(float(row[4]))
+
+print(period1)
+
+
+
+
+
+
