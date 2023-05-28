42e403f5a9cae1c7fc35513020ec6603b3debcab
vtalos <t8210146@aueb.gr>
2023-05-28 18:30:18 +0300

    Improve plot design

--- plots.py
+++ plots.py
@@ -31,7 +31,6 @@ with open("CommitCountsDaily.csv") as csvfile:
         day[i].append(period5[i])
     
 
-# plot the frequencies for each category
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 periods = ["2000-2004", "2005-2009", "2010-2014", "2015-2019", "2020-2023"]
 
@@ -47,7 +46,8 @@ rects5 = ax.bar(x1 + 2*width, period5, width, label=periods[4])
 
 # add labels and titles for the first plot
 ax.set_ylabel('Frequency')
-ax.set_title('Frequency by Category and Time Period')
+ax.set_xlabel('Day')
+ax.set_title('Frequency by Day and Time Period')
 ax.set_xticks(x1)
 ax.set_xticklabels(days)
 ax.legend()
@@ -65,8 +65,12 @@ rects10 = ax2.bar(x2 + 2*width, day[4], width, label=days[4])
 rects11 = ax2.bar(x2 + 3*width, day[5], width, label=days[5])
 rects12 = ax2.bar(x2 + 4*width, day[6], width, label=days[6])
 
-ax2.set_ylabel('')
-ax2.set
+ax2.set_ylabel('Frequencies')
+ax2.set_xlabel('Periods')
+ax2.set_title('Frequency by Time Period for each Day')
+ax2.set_xticks(x2)
+ax2.set_xticklabels(periods)
+ax2.legend()
 
 x3 = np.arange(len(periods))
 width = 0.16
@@ -75,7 +79,11 @@ fig3, ax3 = plt.subplots()
 rects13 = ax3.bar(x3 - 2*width, day[5], width, label=days[5])
 rects14 = ax3.bar(x3 - width, day[6], width, label=days[6])
 
-ax3.set_ylabel('')
-ax3.set
+ax3.set_ylabel('Frequencies')
+ax3.set_xlabel('Periods')
+ax3.set_title('Frequency by Time Period for each Weekend')
+ax3.set_xticks(x3)
+ax3.set_xticklabels(periods)
+ax3.legend()
 
 plt.show()
