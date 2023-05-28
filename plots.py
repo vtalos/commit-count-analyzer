4d5007f21fdf54aeabd03c41abcb0651c331f30c
vtalos <t8210146@aueb.gr>
2023-05-28 18:02:58 +0300

    Add the second plot

--- plots.py
+++ plots.py
@@ -9,11 +9,12 @@ period3 = []
 period4 = []
 period5 = []
 
+day = [[] for _ in range(7)]
+
 # open the csv file and read its contents into the lists
 with open("CommitCountsDaily.csv") as csvfile:
     reader = csv.reader(csvfile)
     next(reader)  # skip header row
-    sum1, sum2, sum3, sum4, sum5 = 0, 0, 0, 0, 0
 
     for row in reader:
         period1.append(float(row[1]))
@@ -21,26 +22,50 @@ with open("CommitCountsDaily.csv") as csvfile:
         period3.append(float(row[3]))
         period4.append(float(row[4]))
         period5.append(float(row[5]))
-
-
+        
+    for i in range(len(period1)):
+        day[i].append(period1[i])
+        day[i].append(period2[i])
+        day[i].append(period3[i])
+        day[i].append(period4[i])
+        day[i].append(period5[i])
+    
 
 # plot the frequencies for each category
-categories = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-x = np.arange(len(categories))
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+periods = ["2000-2004", "2005-2009", "2010-2014", "2015-2019", "2020-2023"]
+
+x1 = np.arange(len(days))
 width = 0.15
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - 2*width, period1, width, label='2000-2004')
-rects2 = ax.bar(x - width, period2, width, label='2005-2009')
-rects3 = ax.bar(x, period3, width, label='2010-2014')
-rects4 = ax.bar(x + width, period4, width, label='2015-2019')
-rects5 = ax.bar(x + 2*width, period5, width, label='2020-2023')
+rects1 = ax.bar(x1 - 2*width, period1, width, label=periods[0])
+rects2 = ax.bar(x1 - width, period2, width, label=periods[1])
+rects3 = ax.bar(x1, period3, width, label=periods[2])
+rects4 = ax.bar(x1 + width, period4, width, label=periods[3])
+rects5 = ax.bar(x1 + 2*width, period5, width, label=periods[4])
 
-# add some labels and titles
+# add labels and titles for the first plot
 ax.set_ylabel('Frequency')
 ax.set_title('Frequency by Category and Time Period')
-ax.set_xticks(x)
-ax.set_xticklabels(categories)
+ax.set_xticks(x1)
+ax.set_xticklabels(days)
 ax.legend()
+
+# create new x values for the second plot
+x2 = np.arange(len(periods))
+width = 0.1
+
+fig2, ax2 = plt.subplots()
+rects6 = ax2.bar(x2 - 2*width, day[0], width, label=days[0])
+rects7 = ax2.bar(x2 - width, day[1], width, label=days[1])
+rects8 = ax2.bar(x2, day[2], width, label=days[2])
+rects9 = ax2.bar(x2 + width, day[3], width, label=days[3])
+rects10 = ax2.bar(x2 + 2*width, day[4], width, label=days[4])
+rects11 = ax2.bar(x2 + 3*width, day[5], width, label=days[5])
+rects12 = ax2.bar(x2 + 4*width, day[6], width, label=days[6])
+
+ax2.set_ylabel('')
+ax2.set
 
 plt.show()
