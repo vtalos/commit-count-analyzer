346e67637dbc6850f0e86767d52a6e5ca8c3f26b
vtalos <t8210146@aueb.gr>
2023-05-29 22:06:51 +0300

    Enrich comment

--- day_plots.py
+++ day_plots.py
@@ -6,7 +6,7 @@ import sys
 
 filename = sys.argv[1] # The first argument is the file name
 plot = sys.argv[2] # The second argument is the requested plot
-# day list contains the number of commits for each time period
+# day list contains the number of commits for each week day in every time period
 day = [[] for _ in range(7)]
 
 sum_period = []
