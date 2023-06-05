845c5f6c9268f769700f5e894499f809524a21a9
vtalos <t8210146@aueb.gr>
2023-06-05 20:15:48 +0300

    Make program more flexible

--- python scripts/commit_analyzer.py
+++ python scripts/commit_analyzer.py
@@ -8,8 +8,10 @@ import argparse
 parser = argparse.ArgumentParser(description="A script for implementing chi square test")
 
 parser.add_argument("filename", help="The csv file to implement chi square test")
+parser.add_argument("test", help="The case to test")
 
-filename = sys.argv[1] # The argument is the file name
+filename = sys.argv[1] # The first argument is the file name
+test = sys.argv[2] # The second argument is the case to test
 
 # open the csv file and read its contents into the lists
 with open(filename) as csvfile:
@@ -30,17 +32,24 @@ for i in range(1, len(period)):
 
 observed_freq = np.transpose(cont_table)
 
-print(observed_freq)
+def chi_square(observed_freq):
+    print(observed_freq)
 
-# perform the chi-square test
-chi2_stat, p_val, dof, expected_freq = chi2_contingency(observed_freq)
+    # perform the chi-square test
+    chi2_stat, p_val, dof, expected_freq = chi2_contingency(observed_freq)
 
-print("Chi-square statistic:", chi2_stat)
-print("P-value:", p_val)
+    print("Chi-square statistic:", chi2_stat)
+    print("P-value:", p_val)
 
-# check the p-value
-if p_val < 0.05:
-    print("The null hypothesis can be rejected. There is a statistically significant relationship between the time period and the observed frequency.")
+    # check the p-value
+    if p_val < 0.05:
+        print("The null hypothesis can be rejected. There is a statistically significant relationship between the time period and the observed frequency.")
+    else:
+        print("The null hypothesis cannot be rejected. There is no statistically significant relationship between the time period and the observed frequency.")
+
+
+if test == "weekends":
+    weekend_freq = observed_freq[-2:]
+    chi_square(weekend_freq)
 else:
-    print("The null hypothesis cannot be rejected. There is no statistically significant relationship between the time period and the observed frequency.")
-
+    chi_square(observed_freq)
