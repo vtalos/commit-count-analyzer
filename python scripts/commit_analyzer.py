da38fde222692c38b1ba55ce586e626a57e99e27
vtalos <t8210146@aueb.gr>
2023-06-05 20:24:34 +0300

    Correct chi-square test

--- python scripts/commit_analyzer.py
+++ python scripts/commit_analyzer.py
@@ -8,10 +8,8 @@ import argparse
 parser = argparse.ArgumentParser(description="A script for implementing chi square test")
 
 parser.add_argument("filename", help="The csv file to implement chi square test")
-parser.add_argument("test", help="The case to test")
 
 filename = sys.argv[1] # The first argument is the file name
-test = sys.argv[2] # The second argument is the case to test
 
 # open the csv file and read its contents into the lists
 with open(filename) as csvfile:
@@ -48,8 +46,4 @@ def chi_square(observed_freq):
         print("The null hypothesis cannot be rejected. There is no statistically significant relationship between the time period and the observed frequency.")
 
 
-if test == "weekends":
-    weekend_freq = observed_freq[-2:]
-    chi_square(weekend_freq)
-else:
-    chi_square(observed_freq)
+chi_square(observed_freq)
