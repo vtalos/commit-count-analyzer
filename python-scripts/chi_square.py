940a3ad98058f31790a7b599f022aa319d9b49c5
vtalos <t8210146@aueb.gr>
2023-08-09 17:02:17 +0300

    Add arguments

--- python-scripts/chi_square.py
+++ python-scripts/chi_square.py
@@ -1,1 +1,11 @@
-print("chi square test")
+import csv
+from itertools import tee
+from scipy.stats import chi2_contingency
+import numpy as np
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="A script for implementing chi square test")
+
+parser.add_argument("filename", help="The csv file to get the data from")
+parser.add_argument("day", help="The day of the week to implement the chi square test")
