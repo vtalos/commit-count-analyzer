93edcb691687632e6bb2adbc33484311552b6f3a
vtalos <t8210146@aueb.gr>
2023-08-13 17:48:14 +0300

    Receive only valid arguments

--- python-scripts/commit_count_per_day.py
+++ python-scripts/commit_count_per_day.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from git import Repo
 import argparse
 import csv
+import sys
 
 # Handle the arguments
 parser = argparse.ArgumentParser(description='Creates a csv containing the commit count per day of the week' \
@@ -11,13 +12,14 @@ parser.add_argument('end_year', type=int, help='The year commit counting stops')
 parser.add_argument('interval', type=int, help='How many years a single interval contains')
 args = parser.parse_args()
 
-repo_path = 'repo\\path'  # Replace with repo path
+repo_path = 'C:\\Users\\zimpr\\OneDrive\\Desktop\\TipMe\\TipMe'  # Replace with repo path
 repo = Repo(repo_path)
 
 days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 # Initialize a dictionary to store the commit counts per day
 num_of_periods = (args.end_year - args.start_year + 1) // args.interval
+
 commit_counts = defaultdict(lambda: [0] * num_of_periods)
 
 # Calculate the commit counts for each day and period
@@ -27,7 +29,14 @@ for commit in repo.iter_commits():
     if args.start_year <= commit_year <= args.end_year:
         day_index = commit.authored_datetime.weekday()
         interval_index = (commit_year - args.start_year) // args.interval
+        
+        if interval_index < 0 or interval_index >= num_of_periods:
+            # Handle cases where commit year is outside the specified range
+            print("Invalid arguments given")
+            sys.exit(1)
+        
         commit_counts[day_index][interval_index] += 1
+
 
 # Write the commit counts in a CSV file
 with open('CommitCountsPerDay.csv', 'w', newline='') as csvfile:
