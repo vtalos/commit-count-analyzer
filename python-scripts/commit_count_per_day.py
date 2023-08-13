c851207622027633b88c56cf503d100c04cdfd93
vtalos <t8210146@aueb.gr>
2023-08-13 16:21:59 +0300

    Calculate commit counts

--- python-scripts/commit_count_per_day.py
+++ python-scripts/commit_count_per_day.py
@@ -10,3 +10,21 @@ parser.add_argument('start_year', type=int, help='The year commit counting start
 parser.add_argument('end_year', type=int, help='The year commit counting stops')
 parser.add_argument('interval', type=int, help='How many years a single interval contains')
 args = parser.parse_args()
+
+repo_path = 'repo\\path'  # Replace with repo path
+repo = Repo(repo_path)
+
+days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+# Initialize a dictionary to store the commit counts per day
+num_of_periods = (args.end_year - args.start_year + 1) // args.interval
+commit_counts = defaultdict(lambda: [0] * num_of_periods)
+
+# Calculate the commit counts for each day and period
+for commit in repo.iter_commits():
+    commit_year = commit.authored_datetime.year
+    
+    if args.start_year <= commit_year <= args.end_year:
+        day_index = commit.authored_datetime.weekday()
+        interval_index = (commit_year - args.start_year) // args.interval
+        commit_counts[day_index][interval_index] += 1
