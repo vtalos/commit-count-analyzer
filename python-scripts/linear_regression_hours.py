38cb494fb7415b8c020c86fd5c3ce3830bc16c21
vtalos <t8210146@aueb.gr>
2023-08-13 18:45:50 +0300

    Apply linear regression

--- python-scripts/linear_regression_hours.py
+++ python-scripts/linear_regression_hours.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import tee
 import sys
+import statsmodels.api as sm
 
 filename = sys.argv[1] # The first argument is the file name
 time_block = int(sys.argv[2]) # The second argument is the time block to keep for the linear regression
@@ -31,3 +32,32 @@ data = []
 for per in period:
     if len(per) > 0:
         data.append(per[time_block])
+
+p = range(1, len(periods))
+print(p, "AAAAAAAAAAAA")
+
+# Add a constant term for the intercept
+X = sm.add_constant(p)
+y = np.array(data)
+
+# Fit the linear regression model
+model = sm.OLS(y, X).fit()
+
+# Print the model summary
+print(model.summary())
+
+# Create a scatter plot of the data points
+plt.scatter(p, y, label="Data Points")
+
+# Plot the linear regression line
+regression_line = model.params[0] + model.params[1] * np.array(p)
+plt.plot(p, regression_line, color='red', label="Regression Line")
+
+plt.xlabel("Period")
+plt.ylabel("Number of Commits")
+plt.title("Linear Regression Analysis")
+plt.legend()
+plt.grid(True)
+# Set integer ticks for the x-axis
+plt.xticks(np.arange(min(p), max(p) + 1, step=1), map(int, np.arange(min(p), max(p) + 1, step=1)))
+plt.show()
