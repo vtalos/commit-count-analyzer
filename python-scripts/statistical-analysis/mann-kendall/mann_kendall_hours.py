607c135dc5bc18a113c2466c2ac985407248d3ab
vtalos <t8210146@aueb.gr>
2023-10-10 21:53:52 +0300

    Read data from csv

--- python-scripts/statistical-analysis/mann-kendall/mann_kendall_hours.py
+++ python-scripts/statistical-analysis/mann-kendall/mann_kendall_hours.py
@@ -7,3 +7,28 @@ import statsmodels.api as sm
 
 filename = sys.argv[1] # The first argument is the file name
 time_block = int(sys.argv[2]) # The second argument is the time block to keep for the linear regression
+
+hours = []
+
+# Open the csv file and read its contents into the lists
+with open(filename) as csvfile:
+    reader = csv.reader(csvfile)
+    periods = next(reader)  # Skip header row
+    # Copy the reader
+    reader_copy1, reader_copy2, reader_copy3 = tee(reader, 3)
+    num_of_periods = len(periods)
+    period = [[] for _ in range(num_of_periods)]
+
+    for row in reader_copy2:
+        for i in range(1, len(period)):
+            period[i].append(float(row[i]))
+
+    # Split day in 1-hour blocks
+    for row in reader_copy3:
+        hours.append(row[0])
+
+data = []
+
+for per in period:
+    if len(per) > 0:
+        data.append(per[time_block])
