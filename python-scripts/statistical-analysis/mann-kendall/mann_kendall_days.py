faa4bd7969773e38ba1542526076e9c1001ee95e
vtalos <t8210146@aueb.gr>
2023-10-10 20:31:23 +0300

    Handle arguments

--- python-scripts/statistical-analysis/mann-kendall/mann_kendall_days.py
+++ python-scripts/statistical-analysis/mann-kendall/mann_kendall_days.py
@@ -1,1 +1,13 @@
 import pymannkendall as mk
+import sys
+import argparse
+from itertools import tee
+import csv
+
+parser = argparse.ArgumentParser(description="A script for implementing Mann Kendall Test")
+
+parser.add_argument("filename", help="The csv file to get the data from")
+parser.add_argument("day", help="The day of the week to implement the Mann Kendall Test")
+
+filename = sys.argv[1] # The first argument is the filename
+week_day = sys.argv[2] # The second argument is the day of the week
