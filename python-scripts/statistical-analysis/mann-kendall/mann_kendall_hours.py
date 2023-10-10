ea8f11d60d949bd5e1be49a679f8a88840a0207c
vtalos <t8210146@aueb.gr>
2023-10-10 21:52:47 +0300

    Read arguments

--- python-scripts/statistical-analysis/mann-kendall/mann_kendall_hours.py
+++ python-scripts/statistical-analysis/mann-kendall/mann_kendall_hours.py
@@ -1,1 +1,9 @@
-print("Mann Kendall for Hours")
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import tee
+import sys
+import statsmodels.api as sm
+
+filename = sys.argv[1] # The first argument is the file name
+time_block = int(sys.argv[2]) # The second argument is the time block to keep for the linear regression
