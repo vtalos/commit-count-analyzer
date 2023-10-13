1eca5b08b76e888df834c2e0f27ae8b230a24418
vtalos <t8210146@aueb.gr>
2023-10-14 02:42:27 +0300

    Handle arguments

--- python-scripts/plots/histogram_per_day.py
+++ python-scripts/plots/histogram_per_day.py
@@ -3,3 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import tee
 import sys
+
+filename = sys.argv[1] # The first argument is the file name
+day = sys.argv[2] # The second argument is the preferred day
+# day list contains the number of commits for each week day in every time period
+day = [[] for _ in range(7)]
