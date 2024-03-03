b8887d7bc92d2796558f97c55aab55a476900282
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2024-03-03 11:02:23 +0200

    Modify kruskall-wallis to show results about pairs

--- python-scripts/statistical-analysis/Kruskal-Wallis/kruskal.py
+++ python-scripts/statistical-analysis/Kruskal-Wallis/kruskal.py
@@ -11,13 +11,15 @@ data=np.array(data)
 groups=[0]*len(data)
 for i in range(0, len(data)):
     groups[i]=data[i] # groups contain the number of commits for each Day/Hour
-    print(groups[i])
-statistic, p_value = kruskal(*groups)
-print("Kruskal-Wallis Test:")
-print("H statistic:", statistic)
-print("p-value:", p_value)
-alpha = 0.05
-if p_value < alpha:
-    print("Reject null hypothesis: There are significant differences between the groups.")
-else:
-    print("Fail to reject null hypothesis: There are no significant differences between the groups.")
+for i in range(0, len(data)):
+    for j in range(0, len(data[i])):
+        if i>j:
+            statistic, p_value = kruskal(groups[i], groups[j])
+            print("Kruskal-Wallis Test:")
+            print("H statistic:", statistic)
+            print("p-value:", p_value)
+            alpha = 0.05
+            if p_value < alpha:
+                print(f'Reject null hypothesis: There are significant differences between the groups {i} and {j}.')
+            else:
+                print(f'Fail to reject null hypothesis: There are no significant differences between the groups {i} and {j}.')
