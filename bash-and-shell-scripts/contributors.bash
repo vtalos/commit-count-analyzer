6dbde1a034f768d35df50c44ce8d516441b5b6f4
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2024-03-19 08:09:13 +0200

    Change date error at contributors.bash

--- bash-and-shell-scripts/contributors.bash
+++ bash-and-shell-scripts/contributors.bash
@@ -8,7 +8,7 @@ for year in {2004..2023}; do
         contributors_per_year=0  
         dir_name="$REPO_LOCATION/$name"	
         cd "$dir_name" || continue
-        contributors_per_year=$(git log --after="$year-01-01" --before="$((year+1))-01-01" --format='%ae' | sort -u | wc -l) 
+        contributors_per_year=$(git log --after="$year-01-01" --before="$year-12-31" --format='%ae' | sort -u | wc -l) 
         contributors_per_year_all_repos=$((contributors_per_year_all_repos + contributors_per_year)) 
     done <"$DATA_LOCATION/projects-accepted.txt"
     echo "$year: $contributors_per_year_all_repos"
