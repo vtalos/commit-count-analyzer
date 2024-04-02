e7e214a311998ed97f644ab869550aa2dd77bd53
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2024-04-02 13:37:39 +0300

    Add diff to check_spike.bash

--- extended-version/check_spike.bash
+++ extended-version/check_spike.bash
@@ -11,8 +11,11 @@ while IFS= read -r name; do
         dir_name="$REPO_LOCATION/$name"
         cd "$dir_name" || continue
         # count only the inserted lines, not the deleted lines
-        commits_at_22=$(git log --after="$year-01-01" --before="$year-12-31" | grep Date | awk '{ print $5 }' | awk '/^22/' | wc -l)
-        echo "name: $name year: $year commits at 22:00-22:59: $commits_at_22"
-        echo "$name: $commits_at_22" >> "$DATA_LOCATION/check_spike.txt"
+        commits_at_22=$(git log --after="$year-01-01" --before="$year-12-31" | grep Date |
+        awk '{ print $5 }' | awk '/^22/' | wc -l)
+        commits_at_22_last_year=$(git log --after="$((year-1))-01-01" --before="$((year-1))-12-31" | grep Date |
+        awk '{ print $5 }' | awk '/^22/' | wc -l)
+        diff= $((commits_at_22 - commits_at_22_last_year))
+        echo "$name: $commits_at_22 $commits_at_22_last_year $diff" >> "$DATA_LOCATION/check_spike.txt"
     done < "$DATA_LOCATION/projects-accepted.txt"
 done
