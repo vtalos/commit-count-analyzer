63afe718c0648e251cfeb64d7fb6a7be33dae542
Evangelos Talos <115378448+vtalos@users.noreply.github.com>
2024-04-05 21:02:41 +0300

    Fix compare_git_svn_commits.bash to count totals

--- extended-version/compare_git_svn_commits.bash
+++ extended-version/compare_git_svn_commits.bash
@@ -6,6 +6,8 @@
 set -eu
 DATA_LOCATION=$(pwd)
 REPO_LOCATION=/home/repos/github
+total_commits_2013=0
+total_commits_excluding_2013=0
 
 while IFS= read -r name; do
     dir_name="$REPO_LOCATION/$name"
@@ -13,11 +15,13 @@ while IFS= read -r name; do
     
     # Count commits with "git-svn-id" in the commit message for 2013
     commits_2013=$(git log --after="2013-01-01" --before="2013-12-31" --grep="git-svn-id" --oneline | wc -l)
+    total_commits_2013=$((total_commits_2013 + commits_2013))
 
     # Count commits with "git-svn-id" in the commit message for the [2005, 2023] period excluding 2013
     commits_excluding_2013=$(git log --after="2005-01-01" --before="2023-12-31" --grep="git-svn-id" --oneline --invert-grep --grep="2013" | wc -l)
+    total_commits_excluding_2013=$((total_commits_excluding_2013 + commits_excluding_2013))
 done < "$DATA_LOCATION/projects-accepted.txt"
 
 # Write results to a text file
-echo "Total commits in 2013 with 'git-svn-id': $commits_2013" > "$DATA_LOCATION/git_svn_id_commits.txt"
-echo "Total commits in [2005, 2023] excluding 2013 with 'git-svn-id': $commits_excluding_2013" >> "$DATA_LOCATION/git_svn_id_commits.txt"
+echo "Total commits in 2013 with 'git-svn-id': $total_commits_2013" > "$DATA_LOCATION/git_svn_id_commits.txt"
+echo "Total commits in [2005, 2023] excluding 2013 with 'git-svn-id': $total_commits_excluding_2013" >> "$DATA_LOCATION/git_svn_id_commits.txt"
