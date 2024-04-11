b223863974974d2ac4e4c83638114c71d8a5ef1f
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2024-04-11 18:11:15 +0300

    Add commit_count_per_hour_extended.py

--- extended-version/scripts/check_spike.bash
+++ extended-version/scripts/check_spike.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 # This script counts the number of commits in a specific hour and year
-# and the commits for the rest of the day
 # for all the projects in the projects-accepted.txt file
 set -eu
 DATA_LOCATION=$(pwd)
@@ -8,20 +7,16 @@ REPO_LOCATION=/home/repos/github
 
 > "$DATA_LOCATION/check_spike.txt"
 total_commits_at_23=0
-total_commits_rest_day=0
 total_diff=0
 year=$1
 echo "$year" >> "$DATA_LOCATION/check_spike.txt"
-echo "commits, commits next year, difference, commits rest of the day" >> "$DATA_LOCATION/check_spike.txt"
+echo "commits, commits next year, difference" >> "$DATA_LOCATION/check_spike.txt"
 while IFS= read -r name; do
         dir_name="$REPO_LOCATION/$name"
         cd "$dir_name" || continue
         # count commints inside 23:00-23:59 for the specific year
         commits_at_23=$(git log --after="$((year-1))-12-31" --before="$((year+1))-01-01"  | grep Date |
         awk '{ print $5 }' | awk '/^23/' | wc -l)
-        # count commints outside 23:00-23:59 for the specific year
-	commits_rest_day=$(git log --after="$((year-1))-12-31" --before="$((year+1))-01-01" | grep Date|
-	awk '{ print $5}' | grep -v '^23' | wc -l)
         #count commits inside 23:00-23:59 for the previous year
         commits_at_23_last_year=$(git log --after="$((year-2))-12-31" --before="$((year))-01-01"  | grep Date |
         awk '{ print $5 }' | awk '/^23/' | wc -l)
@@ -29,7 +24,6 @@ while IFS= read -r name; do
         diff=$((commits_at_23 - commits_at_23_last_year))
 	total_commits_at_23=$((commits_at_23 + total_commits_at_23))
 	total_diff=$((diff + total_diff))
-	total_commits_rest_day=$((commits_rest_day + total_commits_rest_day))
-        echo "$name: $commits_at_23 $commits_at_23_last_year $diff $commits_rest_day" >> "$DATA_LOCATION/check_spike.txt"
+        echo "$name: $commits_at_23 $commits_at_23_last_year $diff" >> "$DATA_LOCATION/check_spike.txt"
 done < "$DATA_LOCATION/projects-accepted.txt"
-echo "total commits 23:00-23:59 $total_commits_at_23 total diff $total_diff total commits rest day $total_commits_rest_day" >> "$DATA_LOCATION/check_spike.txt" 
+echo "total commits 23:00-23:59 $total_commits_at_23 total diff $total_diff" >> "$DATA_LOCATION/check_spike.txt" 
