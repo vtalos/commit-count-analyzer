ea766a5e70ee5d2ea4c011b1319db31f75f986fc
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2024-07-21 21:25:25 +0300

    Modify commits per day

--- write-data-in-csv/commit_count_per_day.py
+++ write-data-in-csv/commit_count_per_day.py
@@ -50,15 +50,15 @@ for repository in repo_list:
         else:
             if non_utc0_commits[(contributor, year)] is None:
                 non_utc0_commits[(contributor, year)] = False
-        for commit in repo.iter_commits():
-            contributor = commit.author.name
-            year = commit.authored_datetime.year
-            if non_utc0_commits[(contributor, year)] == True:
-                day_index = commit.authored_datetime.weekday
-                interval_index = (commit.authored_datetime.year - args.start_year) // args.interval
-                # Increase the commit count in the current hour and period by 1
-                if 0 <= interval_index < num_of_periods:
-                    commit_counts[day_index][interval_index] += 1
+    for commit in repo.iter_commits():
+        contributor = commit.author.name
+        year = commit.authored_datetime.year
+        if non_utc0_commits[(contributor, year)] == True:
+            day_index = commit.authored_datetime.weekday
+            interval_index = (commit.authored_datetime.year - args.start_year) // args.interval
+            # Increase the commit count in the current hour and period by 1
+            if 0 <= interval_index < num_of_periods:
+                commit_counts[day_index][interval_index] += 1
 
 
 def write_counts(args, commit_counts, days_of_week):
